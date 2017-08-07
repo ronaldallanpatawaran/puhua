944612ab54615e497dbeb482ac5ac9ad6ae5c640
ronaldallanpatawaran <ronaldallanpatawaran@gmail.com>
2017-08-07 18:51:54 +0800

    changes

--- admin/config.php
+++ admin/config.php
@@ -28,11 +28,11 @@ define('DIR_CATALOG', BASE_DIR . '/catalog/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_DATABASE', 'ecommerce_puhua');
-define('DB_USERNAME', 'puhua_ronald');
-define('DB_PASSWORD', '4*#)owEDqE^us%');
-define('DB_DATABASE', 'puhua_database');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ecommerce_puhua');
+// define('DB_USERNAME', 'puhua_ronald');
+// define('DB_PASSWORD', '4*#)owEDqE^us%');
+// define('DB_DATABASE', 'puhua_database');
 define('DB_PORT', '3306');
 define('DB_PREFIX', '');
